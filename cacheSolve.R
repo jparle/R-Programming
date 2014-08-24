303dab2c06ed81376ade9827676469e17fc978bc
John Parle <jparle@Johns-MacBook-Air.local>
2014-08-24 14:15:06 +0100

    cacheMatrix.R

---
+++ cacheSolve.R
@@ -0,0 +1,12 @@
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
